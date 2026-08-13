96319875e8858ee8bdefae51980864e15792ff97
Ermowo Seto <ermowo@gmail.com>
2026-08-14 00:12:15 +0700

    Update api/index.js

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const path = require('path');
 const crypto = require('crypto');
 
-const BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
+const BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN || '8048626993:AAFh13Aqhg5P0vZz0Ich-U2sohqAgb53ZaU';
 const ADMIN_CHAT_ID = process.env.ADMIN_CHAT_ID || '2004468668'; 
 const MASTER_SALT = "ERMOWO_UGC_ADS_SECURE_KEY_2026_MASTER";
 const bot = new Telegraf(BOT_TOKEN);
